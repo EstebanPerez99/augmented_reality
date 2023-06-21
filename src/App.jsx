f075bc5b553fb50a78aff4b23a01e153a8d8da46
EstebanPerez99 <epm1520@gmail.com>
2023-06-20 20:10:37 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import Mindar from "./Mindar";
+import img1 from "./assets/img1.jpeg";
+import img2 from "./assets/img2.jpg";
+import img3 from "./assets/img3.jpeg";
+import img4 from "./assets/img4.png";
 
 export default function App() {
 	return (
@@ -7,6 +11,12 @@ export default function App() {
 			<div className='container'>
 				<Mindar />
 				<video></video>
+			</div>
+			<div style={{ position: "absolute", top: 100, left: 100 }}>
+				<img src={img1} alt='ola m' style={{ width: "100px" }} />
+				<img src={img2} alt='ola md' style={{ width: "100px" }} />
+				<img src={img3} alt='olad m' style={{ width: "100px" }} />
+				<img src={img4} alt='olaff m' style={{ width: "100px" }} />
 			</div>
 		</div>
 	);
--- src/Mindar.jsx
+++ src/Mindar.jsx
@@ -1,7 +1,24 @@
 import "aframe";
 import "mind-ar/dist/mindar-image-aframe.prod.js";
 
+import img1 from "./assets/img1.jpeg";
+import img2 from "./assets/img2.jpg";
+import img3 from "./assets/img3.jpeg";
+import img4 from "./assets/img4.png";
+import { useEffect, useState } from "react";
+
 export default function Mindar() {
+	const [idx, setIdx] = useState(0);
+	const images = [img1, img2, img3, img4];
+	useEffect(() => {
+		setInterval(() => {
+			const newImg = idx + 1 >= 4 ? 0 : idx + 1;
+			setIdx(newImg);
+		}, 1000);
+	}, []);
+
+	console.log(idx);
+
 	return (
 		<a-scene
 			mindar-image='imageTargetSrc: ./targets.mind;'
@@ -11,11 +28,12 @@ export default function Mindar() {
 			<a-camera position='0 0 0' look-controls='enabled: false'></a-camera>
 			<a-entity mindar-image-target='targetIndex: 0'>
 				<a-plane
-					color='blue'
+					// color='blue'
+					src={images[idx]}
 					opaciy='0.5'
 					position='0 0 0'
-					height='0.552'
-					width='1'
+					height='1'
+					width='0.5625'
 					rotation='0 0 0'
 				></a-plane>
 			</a-entity>
