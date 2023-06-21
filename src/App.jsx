55a700d464826f50868b69b680b5594719d364a1
EstebanPerez99 <epm1520@gmail.com>
2023-06-21 09:38:15 -0600

    neuva fotos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import Mindar from "./Mindar";
 
-import img1 from "./assets/img1.jpeg";
-import img2 from "./assets/img2.jpg";
-import img3 from "./assets/img3.jpeg";
-import img4 from "./assets/img4.png";
+import img1 from "./assets/img1.JPEG";
+import img2 from "./assets/img2.JPEG";
+import img3 from "./assets/img3.JPEG";
+import img4 from "./assets/img4.JPEG";
+import img5 from "./assets/img5.JPEG";
+import img6 from "./assets/img6.jpg";
+import img7 from "./assets/img6.jpg";
+import img8 from "./assets/img6.jpg";
+import img9 from "./assets/img6.jpg";
+import img10 from "./assets/img6.jpg";
+import img11 from "./assets/img6.jpg";
+import img12 from "./assets/img6.jpg";
+import img13 from "./assets/img6.jpg";
+
 import { useEffect, useState } from "react";
 export default function App() {
 	const [idx, setIdx] = useState(0);
-	const images = [img1, img2, img3, img4];
+	const images = [
+		img1,
+		img2,
+		img3,
+		img4,
+		img5,
+		img6,
+		img7,
+		img8,
+		img9,
+		img10,
+		img11,
+		img12,
+		img13,
+	];
 	useEffect(() => {
 		setTimeout(() => {
-			const newImg = idx + 1 >= 4 ? 0 : idx + 1;
+			const newImg = idx + 1 >= 13 ? 0 : idx + 1;
 			setIdx(newImg);
 		}, 1000);
 	}, [idx]);
